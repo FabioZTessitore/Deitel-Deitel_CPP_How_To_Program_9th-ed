07d9f211679b318eea3757bc163a1e202db64b7b
Fabio Z Tessitore <fabioztessitore@gmail.com>
2023-11-02 08:18:53 +0100

    .

---
+++ chap6/fig06_23.cpp
@@ -0,0 +1,25 @@
+// Fig. 6.23: fig06_23.cpp
+// Overloaded square functions
+
+#include <iostream>
+using namespace std;
+
+int square(int x)
+{
+     cout << "square of integer " << x << " is ";
+     return x * x;
+}
+
+double square(double y)
+{
+     cout << "square of double " << y << " is ";
+     return y * y;
+}
+
+int main()
+{
+     cout << square(7);
+     cout << endl;
+     cout << square(7.5);
+     cout << endl;
+}
---
+++ chap6/fig06_28.cpp
@@ -0,0 +1,23 @@
+// Fig. 6.28: fig06_28.cpp
+// Recursive function factorial
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+unsigned long factorial(unsigned long);
+
+int main()
+{
+     for (unsigned int counter = 0; counter <= 10; ++counter)
+          cout << setw(2) << counter << "! = " << factorial(counter)
+               << endl;
+}
+
+unsigned long factorial(unsigned long number)
+{
+     if (number <= 1)
+          return 1;
+     else
+          return number * factorial(number - 1);
+}
